76b3f0540e91c5a8e180eadb4e1771fe80a84ade
mnabil1718 <mnabil1718@gmail.com>
2023-11-27 04:18:20 +0700

    Fix search empty conditions

--- src/components/search/ProductGridSearch.tsx
+++ src/components/search/ProductGridSearch.tsx
@@ -27,17 +27,15 @@ const ProductGridSearch = async ({
         </p>
       ) : null}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {products.length < 1 ? (
-          <p>No Data...</p>
-        ) : (
-          products.map((product: Product, index: number) => {
-            return (
-              <div className="col-span-1" key={index}>
-                <ProductCard product={product} />
-              </div>
-            );
-          })
-        )}
+        {products.length < 1
+          ? null
+          : products.map((product: Product, index: number) => {
+              return (
+                <div className="col-span-1" key={index}>
+                  <ProductCard product={product} />
+                </div>
+              );
+            })}
       </div>
     </>
   );
